38cc2127cec3631c723ac9313cece3f79b124def
Paul Vint <pjvint@gmail.com>
2015-07-03 10:29:50 -0400

    BG image functionality

--- app/src/main/java/ca/dotslash/pvint/notepic/MainActivity.java
+++ app/src/main/java/ca/dotslash/pvint/notepic/MainActivity.java
@@ -1,19 +1,32 @@
 package ca.dotslash.pvint.notepic;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Context;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.graphics.drawable.Drawable;
 import android.hardware.Camera;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.SurfaceHolder;
+import android.view.View;
 import android.widget.Button;
 import android.view.SurfaceView;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
@@ -21,6 +34,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URI;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
@@ -38,17 +52,163 @@ public class MainActivity extends ActionBarActivity {
     private boolean cameraFront = false;
     private Preview mPreview;
 
+    private ContentValues values;
+    private Intent photoIntent;
+    private ImageView imageView;
+
+    private int screenWidth;
+    private int screenHeight;
+
+    public String photoFilename = "photo.jpeg";
+    public final String APP_TAG = "NotePic";
+
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+    Uri imageUri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         myContext = this;
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        //setContentView(R.layout.activity_main);
-        mPreview = new Preview(this);
-        setContentView(mPreview);
+        setContentView(R.layout.activity_main);
+//        mPreview = new Preview(this);
+//        setContentView(mPreview);
         //initialize();
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        screenWidth = size.x;
+        screenHeight = size.y;
+
+        imageView = (ImageView) findViewById( R.id.imageView );
+        Drawable d = Drawable.createFromPath("/storage/sdcard0/test.jpg");
+        //imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setImageDrawable( d );
+
+        // FIXME TESTING
+         File zfile = new File(Environment.getExternalStorageDirectory()+File.separator + "test.jpg");
+                //Uri uri = Uri.fromFile( file );
+// BOOM        imageView.setImageBitmap(BitmapFactory.decodeFile( "/storage/sdcard0/test.jpg"));
+
+    File imgFile = new  File("/storage/sdcard0/test.jpg");
+    if(imgFile.exists())
+    {
+//        imageView.setAlpha( 0.5f );
+//        imageView.setImageURI(Uri.parse("file://storage/sdcard0/test.jpg"));
+
     }
 
+                //imageView.setImageURI(uri);
+        // DONE FIXME
+
+        // Try just starting camera with an Intent
+        //define the file-name to save photo taken by Camera activity
+
+//create parameters for Intent with filename
+        values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, photoFilename);
+        values.put(MediaStore.Images.Media.DESCRIPTION,"Image capture by camera");
+//imageUri is the current activity attribute, define and save it for later usage (also in onSaveInstanceState)
+        imageUri = getContentResolver().insert(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+//create new Intent
+
+        photoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File file = new File(Environment
+                .getExternalStorageDirectory(),
+                "test.jpg");
+        imageUri = Uri.fromFile(file);
+                            Log.d(APP_TAG, "outputFileUri intent"
+                                    + imageUri);
+                            photoIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                                    imageUri);
+        //photoIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);    // MyFileContentProvider.CONTENT_URI
+        //photoIntent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1);
+        //photoIntent.putExtra(MediaStore.EXTRA_OUTPUT, getPhotoFileUri(photoFilename));
+
+    }
+
+    // Returns the Uri for a photo stored on disk given the fileName
+    public Uri getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        File mediaStorageDir = new File(
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), APP_TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(APP_TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        return Uri.fromFile(new File(mediaStorageDir.getPath() + File.separator + fileName));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d(APP_TAG, RESULT_OK + " Result: " + resultCode);
+        Log.d(APP_TAG, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE + " Request: " + requestCode);
+
+        if (requestCode == 0) { //CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Drawable d = Drawable.createFromPath("/storage/sdcard0/test.jpg");
+                //imageView = (ImageView) findViewById(R.id.imageView);
+                imageView.setImageDrawable( d );
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Cancelled - Picture was not taken", Toast.LENGTH_SHORT).show();
+
+            } else {
+                Toast.makeText(this, "Picture was not taken", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+    public Bitmap decodeSampledBitmapFromUri(Uri uri, int reqWidth,
+                                             int reqHeight) {
+
+        Bitmap bm = null;
+
+        try {
+            // First decode with inJustDecodeBounds=true to check dimensions
+            final BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = true;
+            BitmapFactory.decodeStream(getContentResolver()
+                    .openInputStream(uri), null, options);
+
+            // Calculate inSampleSize
+            options.inSampleSize = calculateInSampleSize(options, reqWidth,
+                    reqHeight);
+
+            // Decode bitmap with inSampleSize set
+            options.inJustDecodeBounds = false;
+            bm = BitmapFactory.decodeStream(getContentResolver()
+                    .openInputStream(uri), null, options);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(), e.toString(),
+                    Toast.LENGTH_LONG).show();
+        }
+
+        return bm;
+    }
+
+    public int calculateInSampleSize(BitmapFactory.Options options,
+                                     int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+            if (width > height) {
+                inSampleSize = Math.round((float) height / (float) reqHeight);
+            } else {
+                inSampleSize = Math.round((float) width / (float) reqWidth);
+            }
+        }
+        return inSampleSize;
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -83,6 +243,8 @@ public class MainActivity extends ActionBarActivity {
 
             case R.id.action_camera:
                 // prompt to take new picture
+
+                startActivityForResult(photoIntent, 0); //CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
                 break;
 
             case R.id.action_save:
@@ -92,6 +254,29 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public static File convertImageUriToFile (Uri imageUri, Activity activity)  {
+        Cursor cursor = null;
+        try {
+            String [] proj={MediaStore.Images.Media.DATA, MediaStore.Images.Media._ID, MediaStore.Images.ImageColumns.ORIENTATION};
+            cursor = activity.managedQuery( imageUri,
+                    proj, // Which columns to return
+                    null,       // WHERE clause; which rows to return (all rows)
+                    null,       // WHERE clause selection arguments (none)
+                    null); // Order-by clause (ascending by name)
+            int file_ColumnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            int orientation_ColumnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.ImageColumns.ORIENTATION);
+            if (cursor.moveToFirst()) {
+                String orientation =  cursor.getString(orientation_ColumnIndex);
+                return new File(cursor.getString(file_ColumnIndex));
+            }
+            return null;
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
     }
 
     public void initialize() {
